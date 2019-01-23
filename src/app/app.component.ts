987ce955ac425fcffdd1f7e75ee2826d5477af42
C.K.Sun <fgriasa123@gmail.com>
2019-01-23 17:44:33 +0800

    Enter

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  url = 'http://blog.miniasp.com/';
+  imgurl = '/assets/images/logo.png';
+  textCount = 0;
+  constructor() {
+  }
+  keySomeThing($event) {
+    console.log(this);
+    if ($event.code === 'Escape') {
+      $event.target.value = '';
+    }
+    this.textCount = $event.target.value.length;
+  }
+  changeTitle(altKey: boolean) {
+    if (altKey) {
+      this.title  = 'The Will Will Web';
+    }
+  }
+}
